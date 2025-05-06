a937a250fa197ed6d5ce663babde5007686e1847
Pawan109 <68591381+Pawan109@users.noreply.github.com>
2025-05-06 17:26:42 +0530

    Time: 15 ms (7.36%) | Memory: 45.8 MB (5.02%) - LeetSync

--- 3309-count-prefix-and-suffix-pairs-i/count-prefix-and-suffix-pairs-i.cpp
+++ 3309-count-prefix-and-suffix-pairs-i/count-prefix-and-suffix-pairs-i.cpp
@@ -1,31 +1,79 @@
-class Solution {
-public:  bool isPrefixAndSuffix(string &check, string &s) {
-        int n = check.length();
+struct trieNode{
+    trieNode * children[26];
+    bool endOfWord;
+};
 
-        //You can use fine() function as well
-        string s1 = s.substr(0, n);
-        string s2 = s.substr(s.length() - n, n);
-        
-        return s1 == check && s2 == check;
-        
+trieNode * getNode(){
+    trieNode * root = new trieNode();
+    root->endOfWord = false;
+
+    for(int i=0;i<26;i++) root->children[i] = NULL;
+
+    return root;
+}
+
+class Trie{
+
+    public : 
+    trieNode * root;
+
+    Trie(){
+        root = getNode();
     }
-    
+
+    void insert(string word){
+        trieNode * crawl = root;
+
+        for(auto ch : word){
+            int ind = ch-'a';
+            if(!crawl->children[ind]) crawl->children[ind] = getNode();
+
+            crawl = crawl->children[ind];
+        }
+        crawl->endOfWord = true;
+    }
+
+    bool searchPrefix(string prefix){
+        trieNode * crawl = root;
+
+        for(auto ch : prefix){ //iterate the prefix
+            int ind = ch-'a';
+            if(!crawl->children[ind]) return false;
+
+            crawl = crawl->children[ind];
+        }
+       return true;
+    }
+};
+
+class Solution {
+public:
     int countPrefixSuffixPairs(vector<string>& words) {
-        // auto lambda = [](string& s1, string& s2) {
-        //     return s1.length() < s2.length();
-        // };
-        
-        //sort(begin(words), end(words), lambda);
-        
+        int n = words.size();
         int count = 0;
-        for(int i = 0; i < words.size()-1; i++) {
-            for(int j = i+1; j < words.size(); j++) {
-                if(words[i].length() <= words[j].length() && isPrefixAndSuffix(words[i], words[j])) {
-                    count++;
-                }
+
+        for( int i=0;i<n;i++){
+            Trie prefixTrie;
+            Trie suffixTrie;
+
+            prefixTrie.insert(words[i]);
+            string rev = words[i];
+
+            reverse(rev.begin(), rev.end());
+
+            suffixTrie.insert(rev);
+        //toh humne s1 ko as a prefix and suffix daal diya Tries mein 
+        //ab s2 ke saath dono ends check karne hai bss
+        
+            for(int j=0;j<i;j++){
+                if(words[j].size() > words[i].size()) continue;
+
+                string rev2 = words[j];
+                reverse(rev2.begin() , rev2.end());
+
+                if(prefixTrie.searchPrefix(words[j]) && suffixTrie.searchPrefix(rev2) ) count++;
             }
         }
-        
         return count;
     }
 };
